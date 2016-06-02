217ea20d229adb8119cfb50a2d520484367ba242
Dominik Muhs <dominik.muhs@mailbox.tu-dresden.de>
2016-06-01 22:36:48 -0500

    Add paste_limit parameter to limit amount of pastes

--- scraper.py
+++ scraper.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import queue
-import random
 import requests
 import threading
 import time
@@ -9,42 +8,44 @@ from lxml import html
 
 
 class PastebinScraper(object):
-    def __init__(self):
+    def __init__(self, paste_limit):
         # TODO: Paste limit
         # TODO: Resilient requests import
         # TODO: Requests status code and reason
         # TODO: DB connector
         # TODO: Unlimited pastes
+        self.paste_limit = paste_limit
         self.PB_LINK = 'http://pastebin.com/'
         self.pastes = queue.Queue(maxsize=8)
         self.pastes_seen = set()
         self.workers = 2
 
-    def _parse_page_content(self):
-        # TODO: Make import more resilient
-        # TODO: page.status_code + page.reason
-        page = requests.get(self.PB_LINK)
-        tree = html.fromstring(page.content)
-        return tree.cssselect('ul.right_menu li')
-
     def _get_paste_data(self):
-        pastes = self._parse_page_content()
-        for paste in pastes:
-            name_link = paste.cssselect('a')[0]
-            name = name_link.text_content()
-            href = name_link.get('href')[1:]  # Get rid of leading /
-            data = paste.cssselect('span')[0].text_content().split('|')
-            language = None
-            if len(data) == 2:
-                # Got language
-                language = data[0]
-            paste_data = (name, language, href)
-            if paste_data not in self.pastes:
-                # New paste detected
-                self.pastes.put(paste_data)
-                self.paste_counter += 1
-                delay = random.randrange(1, 5)
-                time.sleep(delay)
+        paste_counter = 0
+        while paste_counter < self.paste_limit:
+            page = requests.get(self.PB_LINK)
+            tree = html.fromstring(page.content)
+            pastes = tree.cssselect('ul.right_menu li')
+            for paste in pastes:
+                if paste_counter >= self.paste_limit:
+                    # Break for limits % 8 != 0
+                    break
+                name_link = paste.cssselect('a')[0]
+                name = name_link.text_content()
+                href = name_link.get('href')[1:]  # Get rid of leading /
+                data = paste.cssselect('span')[0].text_content().split('|')
+                language = None
+                if len(data) == 2:
+                    # Got language
+                    language = data[0]
+                paste_data = (name, language, href)
+                if paste_data[2] not in self.pastes_seen:
+                    # New paste detected
+                    self.pastes_seen.add(paste_data[2])
+                    self.pastes.put(paste_data)
+                    delay = 1  # random.randrange(1, 5)
+                    time.sleep(delay)
+                    paste_counter += 1
 
     def _download_paste(self):
         while True:
@@ -73,5 +74,5 @@ class PastebinScraper(object):
 
 
 if __name__ == '__main__':
-    ps = PastebinScraper()
+    ps = PastebinScraper(paste_limit=2)
     ps.run()
